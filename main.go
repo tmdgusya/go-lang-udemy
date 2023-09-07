54e7c77b7f445200b4a4b0eed608381f12b5161e
tmdgusya <tmdgusya@naver.com>
2023-09-08 00:44:32 +0900

    Declare variable

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare string variable
+	// and then assign string value into it.
+	// var is short for variable
+	var card string = "Ace of Spades"
+	fmt.Println(card)
+}
