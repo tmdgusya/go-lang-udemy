5f86f18f52d7b4e3b937c1525e81d47e1e1da410
tmdgusya <tmdgusya@naver.com>
2023-09-09 00:54:42 +0900

    Made a newDeck function to make deck easily

--- deck.go
+++ deck.go
@@ -4,6 +4,36 @@ package main
 // which is a slices of string.
 type deck []string
 
+/*
+ * Return the list of cards are consist of string
+ */
+func newDeck() deck {
+	cards := deck{}
+
+	// we're not making this a deck because a deck is really supposed to be used with actual playing cards.
+	cardSuits := []string{
+		"Spades",
+		"Diamonds",
+		"Hearts",
+		"Clubs",
+	}
+
+	cardValues := []string{
+		"Ace",
+		"Two",
+		"Three",
+		"Four",
+	}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		println(i, card)
--- main.go
+++ main.go
@@ -1,19 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	// declare string variable
 	// and then assign string value into it.
 	// var is short for variable
-	var card string = newCard()
-	var cards deck = deck{
-		newCard(), newCard(),
-	}
-	strings := append(cards, card) // this does not modify the original slices.
-	fmt.Println(cards)
-	fmt.Println(strings)
-
+	var cards deck = newDeck()
 	cards.print()
 }
 
