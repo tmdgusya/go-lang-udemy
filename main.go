e9a77aad22a1963bcf8201755eb4d678023529ba
tmdgusya <dev0jsh@gmail.com>
2023-09-08 10:08:24 +0900

    studying how to use slices in GO

--- main.go
+++ main.go
@@ -6,6 +6,19 @@ func main() {
 	// declare string variable
 	// and then assign string value into it.
 	// var is short for variable
-	var card string = "Ace of Spades"
-	fmt.Println(card)
+	var card string = newCard()
+	var cards []string = []string{
+		newCard(), newCard(),
+	}
+	strings := append(cards, card) // this does not modify the original slices.
+	fmt.Println(cards)
+	fmt.Println(strings)
+
+	for i, card := range strings {
+		println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
