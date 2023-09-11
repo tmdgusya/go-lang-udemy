f51da6de3aa29559d964d8ee78089a3e91b268e4
tmdgusya <dev0jsh@gmail.com>
2023-09-11 21:53:06 +0900

    Make an embedded struct

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	alex := person{
-		firstName: "Alex",
-		lastName:  "Anderson",
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@party.com",
+			zipCode: 50500,
+		},
 	}
-	print(alex.firstName)
+	fmt.Printf("%+v", jim)
 }
